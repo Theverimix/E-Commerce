89d5daa6e394deaad6cc99ca18eaa3e217f0a854
Theverimix <113361179+Theverimix@users.noreply.github.com>
2024-02-29 20:42:12 -0300

    Successful database connection

--- Backend Springboot/e-commerce/src/main/java/com/ecommerce/entities/Product.java
+++ Backend Springboot/e-commerce/src/main/java/com/ecommerce/entities/Product.java
@@ -1,8 +1,24 @@
 package com.ecommerce.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "products")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String name;
 }
