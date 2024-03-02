3f3ebd991182d85c6ae5efa02baee28e5dfaceb2
Gabriel De Leon <xgabrieldeleon@gmail.com>
2024-03-02 16:50:24 -0300

    Added basic REST for Product

---
+++ Backend Springboot/e-commerce/src/main/java/com/ecommerce/controller/ProductController.java
@@ -0,0 +1,52 @@
+package com.ecommerce.controller;
+
+import com.ecommerce.entities.Product;
+import com.ecommerce.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts(){
+        List<Product> products = productService.getAllProducts();
+        return ResponseEntity.ok(products);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Product>> getProductById(@PathVariable Long id) {
+        Optional<Product> product = productService.getProductById(id);
+        return ResponseEntity.ok(product);
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> saveProduct(@RequestBody Product product){
+        Product newProduct = productService.saveProduct(product);
+        return ResponseEntity.ok(newProduct);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product newProduct){
+        Product product = productService.updateProduct(id, newProduct);
+        return ResponseEntity.ok(product);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteProduct(@PathVariable Long id){
+        productService.deleteProduct(id);
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ Backend Springboot/e-commerce/src/main/java/com/ecommerce/services/ProductService.java
@@ -0,0 +1,55 @@
+package com.ecommerce.services;
+
+import com.ecommerce.entities.Product;
+import com.ecommerce.repositories.ProductRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Optional<Product> getProductById(Long id){
+        return productRepository.findById(id);
+    }
+
+    public List<Product> getAllProducts(){
+        return productRepository.findAll();
+    }
+
+    public Product saveProduct(Product product){
+        return productRepository.save(product);
+    }
+
+    public Product updateProduct(Long id, Product newProduct){
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Entity not found with id: " + id));
+        product.setName(newProduct.getName());
+        product.setDescription(newProduct.getDescription());
+        product.setPrice(newProduct.getPrice());
+        product.setStock(newProduct.getStock());
+        //product.setCreated_Date(newProduct.getCreated_Date());
+        //product.setState(newProduct.getState());
+        product.setVisible(newProduct.isVisible());
+        //product.setImages(newProduct.getImages());
+        //product.setProductCategories(newProduct.getProductCategories());
+        //product.setProductSales(newProduct.getProductSales());
+        return productRepository.save(product);
+    }
+
+    public void deleteProduct(Long id){
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Entity not found with id: " + id));
+        productRepository.delete(product);
+    }
+}
