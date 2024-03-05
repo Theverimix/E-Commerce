65bcbc6627f567873501901ed23a7e5f91f00c0b
Gabriel De Leon <xgabrieldeleon@gmail.com>
2024-03-05 16:31:43 -0300

    Fixes

---
+++ Backend Springboot/e-commerce/src/test/java/com/ecommerce/controller/UserControllerTest.java
@@ -0,0 +1,8 @@
+package com.ecommerce.controller;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(UserController.class)
+public class UserControllerTest {
+
+}
