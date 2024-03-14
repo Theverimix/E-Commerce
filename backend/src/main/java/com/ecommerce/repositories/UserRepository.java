bd07e93b06bc1c2554439e86fcff0eebc05403dd
Gabriel De León <83258605+GabrielDLeon@users.noreply.github.com>
2024-03-14 19:26:30 -0300

    Added Spring Security + JWT dependency

--- backend/src/main/java/com/ecommerce/repositories/UserRepository.java
+++ backend/src/main/java/com/ecommerce/repositories/UserRepository.java
@@ -2,10 +2,15 @@ package com.ecommerce.repositories;
 
 import com.ecommerce.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
 
     // default List<User> findByCriteria(String name, String password, String email,
     // UsersStates state, UsersRoles role) {
