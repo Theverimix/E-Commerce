1165b0deb6cd5bd56c30a6d245725d774e43f277
Theverimix <113361179+Theverimix@users.noreply.github.com>
2024-03-18 15:52:09 -0300

    Fix SecurityFilterChain

--- backend/src/main/java/com/ecommerce/config/SecurityConfig.java
+++ backend/src/main/java/com/ecommerce/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.ecommerce.config;
 
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -9,29 +11,33 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final AuthenticationProvider authProvider;
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+        private final AuthenticationProvider authProvider;
+        private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        return http
-                .csrf((csrf) -> csrf.disable())
-                /*
-                .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("/api/auth/**").permitAll()
-                        .anyRequest().authenticated()
-                )
-                 */
-                .authorizeHttpRequests((authorize) -> authorize
-                        .anyRequest().permitAll()
-                )
-                .authenticationProvider(authProvider)
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                .build();
-    }
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+                return http
+                                .csrf(csrf -> csrf.disable())
+                                .authorizeHttpRequests(authorize -> authorize
+                                                .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+                                                .permitAll()
+                                                .requestMatchers(RequestMatcherUtil::isApiAuthRequest).permitAll()
+                                                .anyRequest().authenticated())
+                                .authenticationProvider(authProvider)
+                                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                                .build();
+        }
+}
+
+class RequestMatcherUtil {
+        public static boolean isApiAuthRequest(HttpServletRequest request) {
+                return request.getRequestURI().startsWith("/api/auth/");
+        }
 }
