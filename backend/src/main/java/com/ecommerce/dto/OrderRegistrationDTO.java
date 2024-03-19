88572fb7ceddf6d752793d298420f10b83c8ba24
Theverimix <113361179+Theverimix@users.noreply.github.com>
2024-03-19 17:12:47 -0300

    Fix Dto imports

--- backend/src/main/java/com/ecommerce/dto/OrderRegistrationDTO.java
+++ backend/src/main/java/com/ecommerce/dto/OrderRegistrationDTO.java
@@ -4,8 +4,10 @@ import com.ecommerce.entities.OrderDetails;
 
 import java.util.Set;
 
+import org.hibernate.validator.constraints.NotBlank;
+
 public record OrderRegistrationDTO(
-                Long customerId,
-                @NotBlank String address,
-                Set<OrderDetails> details) {
+        Long customerId,
+        @NotBlank String address,
+        Set<OrderDetails> details) {
 }
--- backend/src/main/java/com/ecommerce/dto/ProductRegistrationDTO.java
+++ backend/src/main/java/com/ecommerce/dto/ProductRegistrationDTO.java
@@ -5,13 +5,17 @@ import com.ecommerce.entities.Category;
 import java.util.List;
 import java.util.Set;
 
+import javax.validation.constraints.Min;
+
+import org.hibernate.validator.constraints.NotBlank;
+
 public record ProductRegistrationDTO(
-                @NotBlank @Min(3) String name,
-                @NotBlank @Min(3) String description,
-                @NotBlank double price,
-                @NotBlank int stock,
-                @NotBlank Long idState,
-                @NotBlank boolean visible,
-                List<String> images,
-                Set<Category> productCategories) {
+        @NotBlank @Min(3) String name,
+        @NotBlank @Min(3) String description,
+        @NotBlank double price,
+        @NotBlank int stock,
+        @NotBlank Long idState,
+        @NotBlank boolean visible,
+        List<String> images,
+        Set<Category> productCategories) {
 }
