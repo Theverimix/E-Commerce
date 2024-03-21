90d3cccebdb33cfebb89f5787d9ba37cab80d947
Theverimix <113361179+Theverimix@users.noreply.github.com>
2024-03-21 18:20:29 -0300

    Fix spring-boot-starter-validation dependency

--- backend/src/main/java/com/ecommerce/dto/ProductRegistrationDTO.java
+++ backend/src/main/java/com/ecommerce/dto/ProductRegistrationDTO.java
@@ -2,20 +2,19 @@ package com.ecommerce.dto;
 
 import com.ecommerce.entities.Category;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
 import java.util.List;
 import java.util.Set;
 
-import javax.validation.constraints.Min;
-
-import org.hibernate.validator.constraints.NotBlank;
-
 public record ProductRegistrationDTO(
-        @NotBlank @Min(3) String name,
-        @NotBlank @Min(3) String description,
-        @NotBlank double price,
-        @NotBlank int stock,
-        @NotBlank Long idState,
-        @NotBlank boolean visible,
-        List<String> images,
-        Set<Category> productCategories) {
+                @NotBlank @Min(3) String name,
+                @NotBlank @Min(3) String description,
+                @NotBlank double price,
+                @NotBlank int stock,
+                @NotBlank Long idState,
+                @NotBlank boolean visible,
+                List<String> images,
+                Set<Category> productCategories) {
 }
