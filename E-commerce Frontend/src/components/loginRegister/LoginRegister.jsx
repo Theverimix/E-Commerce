f9d76953603cd40cfb2096fefb2ba8225890e3c4
Theverimix <113361179+Theverimix@users.noreply.github.com>
2024-03-21 16:21:02 -0300

    Frontend and Backend basic connection

--- E-commerce Frontend/src/components/loginRegister/LoginRegister.jsx
+++ E-commerce Frontend/src/components/loginRegister/LoginRegister.jsx
@@ -1,18 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card } from 'primereact/card';
 import { Divider } from 'primereact/divider';
 import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
 import headerImage from '../../assets/img/ec-girl-banner.jpg';
+import axios from 'axios';
 
 export default function LoginRegister() {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const userLogin = async () => {
+        
+        try {
+          const response = await axios.post('http://localhost:8080/api/auth/login', {
+            email: username,
+            password: password
+          });
+          console.log('Login successful:', response.data);
+          alert("Success")
+          // Aquí podrías redirigir al usuario a otra página después de iniciar sesión correctamente
+        } catch (error) {
+          console.error('Login failed:', error);
+          alert("Failed")
+          // Aquí podrías mostrar un mensaje de error al usuario
+        }
+      };
+
+
+
+
+
     const headerLogin = (
         <img alt="Card" src={headerImage} />
 
     );
     const footerLogin = (
         <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-            <Button label="Log in" icon="pi pi-check" />
+            <Button label="Log in" icon="pi pi-check" onClick={userLogin}/>
         </div>
     );
 
@@ -41,7 +67,7 @@ export default function LoginRegister() {
                         <span className="p-inputgroup-addon">
                             <i className="pi pi-user"></i>
                         </span>
-                        <InputText placeholder="Username" />
+                        <InputText placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)}/>
                     </div>
                     <br />
 
@@ -50,7 +76,7 @@ export default function LoginRegister() {
                         <span className="p-inputgroup-addon">
                             <i className="pi pi-key"></i>
                         </span>
-                        <InputText placeholder="Password" />
+                        <InputText placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
                     </div>
                 </div>
             </Card>
--- backend/src/main/java/com/ecommerce/config/SecurityConfig.java
+++ backend/src/main/java/com/ecommerce/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.ecommerce.config;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.Arrays;
+
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +12,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -34,6 +39,18 @@ public class SecurityConfig {
                                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                                 .build();
         }
+
+        // @Bean
+        // CorsConfigurationSource corsConfigurationSource() {
+        // CorsConfiguration configuration = new CorsConfiguration();
+        // configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        // configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT",
+        // "DELETE"));
+        // UrlBasedCorsConfigurationSource source = new
+        // UrlBasedCorsConfigurationSource();
+        // source.registerCorsConfiguration("/**", configuration);
+        // return source;
+        // }
 }
 
 class RequestMatcherUtil {
